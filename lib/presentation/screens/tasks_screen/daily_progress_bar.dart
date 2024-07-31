4d2bbd5a16dc4b302dc75c55a8af397caaeb56ac
Alexrospersonal <alexrospersonal@gmail.com>
2024-07-31 20:55:24 +0300

    Add diagram week progress widget

--- lib/presentation/screens/tasks_screen/daily_progress_bar.dart
+++ lib/presentation/screens/tasks_screen/daily_progress_bar.dart
@@ -1,10 +1,15 @@
 import 'dart:math';
 import 'dart:ui' as ui;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_todo_project/generated/l10n.dart';
 import 'package:flutter_todo_project/presentation/styles/theme_styles.dart';
 import 'package:intl/intl.dart';
+import 'package:isar/isar.dart';
 
 class DailyProgressBar extends StatefulWidget {
   const DailyProgressBar({super.key});
@@ -37,11 +42,208 @@ class _DailyProgressBarState extends State<DailyProgressBar> {
           ],
         ),
         if (isExpanded)
-          const ExpandedCircleProgressDetailsBar(
-            percent: 0.66,
+          Row(
+            children: [
+              const ExpandedCircleProgressDetailsBar(
+                percent: 0.66,
+              ),
+              const ExpandedDiagramWeekDetails()
+            ],
           )
       ],
     );
+  }
+}
+
+class ExpandedDiagramWeekDetails extends StatelessWidget {
+  const ExpandedDiagramWeekDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 180,
+      height: 140,
+      decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+      child: CustomPaint(
+        painter: AnimatedWeekProgressDiagramm(progressInWeek: [0.75, 1, 0.23, 1, 0.56, 0.66, 0.89]),
+      ),
+    );
+  }
+}
+
+class AnimatedWeekProgressDiagramm extends CustomPainter {
+  final List<double> progressInWeek;
+
+  AnimatedWeekProgressDiagramm({required this.progressInWeek});
+
+  ui.ParagraphBuilder createParagraphBuilder(ui.TextStyle textStyle, double fontSize) {
+    ui.ParagraphBuilder paragraphBuilder = ui.ParagraphBuilder(ui.ParagraphStyle(fontSize: fontSize, fontWeight: FontWeight.w500));
+    paragraphBuilder.pushStyle(textStyle);
+
+    return paragraphBuilder;
+  }
+
+  ui.Paragraph getParagraph(ui.ParagraphBuilder paragraphBuilder, double constraintWidth) {
+    ui.Paragraph paragraph = paragraphBuilder.build();
+    paragraph.layout(ui.ParagraphConstraints(width: constraintWidth));
+
+    return paragraph;
+  }
+
+  ui.ParagraphBuilder addText(String text) {
+    var builder = createParagraphBuilder(
+      textStyle,
+      10,
+    );
+
+    builder.addText(text);
+
+    return builder;
+  }
+
+  var textStyle = ui.TextStyle(color: greyColor, fontSize: 10, height: 1);
+
+  double calculatePercentage(double percent) {
+    // Значення на 0% та 100%
+    double minValue = 112.4; // Значення при 0%
+    double maxValue = 10; // Значення при 100%
+
+    // Лінійна інтерполяція
+    double value = minValue + (maxValue - minValue) * (percent / 100);
+
+    return value;
+  }
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    double column = size.width / 4 - 15;
+    double row = size.height / 5 - 1;
+    double lineWidth = size.width - column - 10;
+    double weekColumn = lineWidth / 7;
+
+    Paint line = Paint();
+    line.color = greyColor;
+    line.style = PaintingStyle.stroke;
+    line.strokeWidth = 1;
+
+    Paint diagramLine = Paint();
+    diagramLine.style = PaintingStyle.stroke;
+    diagramLine.color = Colors.black;
+    diagramLine.strokeCap = StrokeCap.round;
+    diagramLine.strokeWidth = 12;
+
+    double x = column + weekColumn - 4;
+
+    double startY = size.height - row;
+
+    Rect rect = const Rect.fromLTRB(0, 0, 180, 140);
+
+    final Gradient gradient = LinearGradient(
+      begin: Alignment.bottomCenter,
+      end: Alignment.topCenter,
+      colors: const [
+        onPrimaryColor,
+        primaryColorWithOpacity,
+        primaryColor,
+      ],
+      stops: const [0.2, 0.6, 1],
+    );
+
+    diagramLine.shader = gradient.createShader(rect);
+
+    canvas.drawLine(
+      Offset(x, startY),
+      Offset(x, calculatePercentage(progressInWeek[0] * 100)),
+      diagramLine,
+    );
+
+    canvas.drawLine(
+      Offset(x + weekColumn * 1, startY),
+      Offset(x + weekColumn * 1, calculatePercentage(progressInWeek[1] * 100)),
+      diagramLine,
+    );
+
+    canvas.drawLine(
+      Offset(x + weekColumn * 2, startY),
+      Offset(x + weekColumn * 2, calculatePercentage(progressInWeek[2] * 100)),
+      diagramLine,
+    );
+
+    canvas.drawLine(
+      Offset(x + weekColumn * 3, startY),
+      Offset(x + weekColumn * 3, calculatePercentage(progressInWeek[3] * 100)),
+      diagramLine,
+    );
+
+    canvas.drawLine(
+      Offset(x + weekColumn * 4, startY),
+      Offset(x + weekColumn * 4, calculatePercentage(progressInWeek[4] * 100)),
+      diagramLine,
+    );
+
+    canvas.drawLine(
+      Offset(x + weekColumn * 5, startY),
+      Offset(x + weekColumn * 5, calculatePercentage(progressInWeek[5] * 100)),
+      diagramLine,
+    );
+
+    canvas.drawLine(
+      Offset(x + weekColumn * 6, startY),
+      Offset(x + weekColumn * 6, calculatePercentage(progressInWeek[6] * 100)),
+      diagramLine,
+    );
+
+    var percent0 = getParagraph(addText("0%"), 15);
+    canvas.drawParagraph(percent0, Offset(column - percent0.width, (size.height - row) - percent0.height / 2));
+    canvas.drawLine(Offset(column, size.height - row), Offset(size.width, size.height - row), line);
+
+    var percent25 = getParagraph(addText("25%"), 20);
+    canvas.drawParagraph(percent25, Offset(column - percent25.width, (size.height - row * 2) - percent25.height / 2));
+    canvas.drawLine(Offset(column, size.height - row * 2), Offset(size.width, size.height - row * 2), line);
+
+    var percent50 = getParagraph(addText("50%"), 20);
+    canvas.drawParagraph(percent50, Offset(column - percent50.width, (size.height - row * 3) - percent50.height / 2));
+    canvas.drawLine(Offset(column, size.height - row * 3), Offset(size.width, size.height - row * 3), line);
+
+    var percent75 = getParagraph(addText("75%"), 20);
+    canvas.drawParagraph(percent75, Offset(column - percent75.width, (size.height - row * 4) - percent75.height / 2));
+    canvas.drawLine(Offset(column, size.height - row * 4), Offset(size.width, size.height - row * 4), line);
+
+    var percent100 = getParagraph(addText("100%"), 25);
+    canvas.drawParagraph(percent100, Offset(column - percent100.width, (size.height - row * 5) - percent100.height / 2));
+    canvas.drawLine(Offset(column, size.height - row * 5), Offset(size.width, size.height - row * 5), line);
+
+    canvas.save(); // Зберігає поточний стан
+    canvas.translate(size.width, size.height); // Перемістіть у центр
+    canvas.rotate(-pi / 2);
+
+    var mon = getParagraph(addText("Mon"), 20);
+    canvas.drawParagraph(mon, Offset(2, weekColumn * -7));
+
+    var tue = getParagraph(addText("Tue"), 20);
+    canvas.drawParagraph(tue, Offset(5, weekColumn * -6));
+
+    var wen = getParagraph(addText("Wen"), 20);
+    canvas.drawParagraph(wen, Offset(2, weekColumn * -5));
+
+    var thu = getParagraph(addText("Thu"), 20);
+    canvas.drawParagraph(thu, Offset(4, weekColumn * -4));
+
+    var fri = getParagraph(addText("Fri"), 20);
+    canvas.drawParagraph(fri, Offset(10, weekColumn * -3));
+
+    var sat = getParagraph(addText("Sat"), 20);
+    canvas.drawParagraph(sat, Offset(7, weekColumn * -2));
+
+    var sun = getParagraph(addText("Sun"), 20);
+    canvas.drawParagraph(sun, Offset(5, weekColumn * -1));
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
 
@@ -53,8 +255,7 @@ class ExpandedCircleProgressDetailsBar extends StatefulWidget {
   State<ExpandedCircleProgressDetailsBar> createState() => _ExpandedCircleProgressDetailsBarState();
 }
 
-class _ExpandedCircleProgressDetailsBarState extends State<ExpandedCircleProgressDetailsBar>
-    with SingleTickerProviderStateMixin {
+class _ExpandedCircleProgressDetailsBarState extends State<ExpandedCircleProgressDetailsBar> with SingleTickerProviderStateMixin {
   late AnimationController controller;
   late Animation<double> gradientAnimation;
   late Animation<double> percentAnimation;
@@ -181,31 +382,29 @@ class MyPainter extends CustomPainter {
     filedPainter.color = Colors.yellow;
     filedPainter.style = PaintingStyle.stroke;
     filedPainter.strokeWidth = 5;
-    canvas.drawArc(Offset(5.5, 5.5) & Size(size.width - 5 - 6, size.height - 5 - 6), pi * 2 * percent - (pi / 2),
-        pi * 2 * (1.0 - percent), false, filedPainter);
+    canvas.drawArc(Offset(5.5, 5.5) & Size(size.width - 5 - 6, size.height - 5 - 6), pi * 2 * percent - (pi / 2), pi * 2 * (1.0 - percent),
+        false, filedPainter);
 
     final feelPainter = Paint();
     feelPainter.color = Colors.blue;
     feelPainter.style = PaintingStyle.stroke;
     feelPainter.strokeWidth = 5;
     feelPainter.strokeCap = StrokeCap.round;
-    canvas.drawArc(Offset(5.5, 5.5) & Size(size.width - 5 - 6, size.height - 5 - 6), -pi / 2, pi * 2 * percent, false,
-        feelPainter);
 
-    // ui.TextStyle textStyle = ui.TextStyle(color: Colors.red);
+    canvas.drawArc(Offset(5.5, 5.5) & Size(size.width - 5 - 6, size.height - 5 - 6), -pi / 2, pi * 2 * percent, false, feelPainter);
 
-    // ui.ParagraphBuilder paragraphBuilder = ui.ParagraphBuilder(
-    //     ui.ParagraphStyle(fontSize: 18, fontWeight: FontWeight.bold));
-    // paragraphBuilder.pushStyle(textStyle);
+    ui.TextStyle textStyle = ui.TextStyle(color: Colors.red);
 
-    // paragraphBuilder.addText("100%");
+    ui.ParagraphBuilder paragraphBuilder = ui.ParagraphBuilder(ui.ParagraphStyle(fontSize: 18, fontWeight: FontWeight.bold));
+    paragraphBuilder.pushStyle(textStyle);
 
-    // ui.Paragraph paragraph = paragraphBuilder.build();
-    // ui.ParagraphConstraints paragraphConstraints =
-    //     ui.ParagraphConstraints(width: size.width);
-    // paragraph.layout(ui.ParagraphConstraints(width: size.width));
+    paragraphBuilder.addText("100%");
 
-    // canvas.drawParagraph(paragraph, Offset(size.width / 2, size.height / 2));
+    ui.Paragraph paragraph = paragraphBuilder.build();
+    ui.ParagraphConstraints paragraphConstraints = ui.ParagraphConstraints(width: size.width);
+    paragraph.layout(ui.ParagraphConstraints(width: size.width));
+
+    canvas.drawParagraph(paragraph, Offset(size.width / 2, size.height / 2));
   }
 
   @override
